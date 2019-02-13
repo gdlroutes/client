e0fb70c1b0c4467624c1a34badf8ceaf4f8619f2
chuyalvarez <jesusaagomez@hotmail.com>
2019-02-12 22:12:48 -0600

    integration complete to Go API

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,78 +33,15 @@ export default {
   data: () => ({
     map: null,
     tileLayer: null,
-    layers: [
-      {
-          "id":0,
-          "name":"Libraries",
-          "active":false,
-          "features":[
-              {
-                  "id": 13,
-                  "name": "Biblioteca Central Estatal Profesor Ram\u00f3n Garc\u00eda Ruiz",
-                  "coords":[20.6868385,-103.35037],
-                  "type":"marker"
-              },
-              {
-                  "id": 14,
-                  "name": "Biblioteca Iberoamericana Octavio Paz",
-                  "coords":[20.6756959,-103.3483031],
-                  "type":"marker"
-              },
-              {
-                  "id": 15,
-                  "name": "Biblioteca del Ej\u00e9rcito y Fuerza A\u00e9rea Mexicana",
-                  "coords":[20.6862386,-103.3511596],
-                  "type":"marker"
-              },
-              {
-                  "id": 16,
-                  "name": "Biblioteca P\u00fablica del Estado de Jalisco",
-                  "coords":[20.738445,-103.3813623],
-                  "type":"marker"
-              },
-          ]
-      },
-      {
-          "id":1,
-          "name":"Bosques",
-          "active":false,
-          "features":[
-              {
-                  "id":17,
-                  "name":"\u00c1rea de Protecci\u00f3n de Flora y Fauna La Primavera",
-                  "type":"marker",
-                  "coords":[20.7011129,-103.5781066],
-              },
-              {
-                  "id":18,
-                  "name":"Bosque El Centinela",
-                  "type":"marker",
-                  "coords":[20.760076,-103.3823272],
-              },
-              {
-                  "id":19,
-                  "name":"Bosque Los Colomos (Ingreso El Chaco)",
-                  "type":"marker",
-                  "coords":[20.7040375,-103.3899401],
-              },
-              {
-                  "id":20,
-                  "name":"Bosque Los Colomos (Ingreso Paseo Torre\u00f3n)",
-                  "type":"marker",
-                  "coords":[20.7060597,-103.3939649],
-              },
-          ],
-      },
-  ],
+    layers: [],
   }),
   mounted() {
     this.initMap();
-    this.initLayers();
+    this.getLandmarks();
   },
   methods: {
     initMap() {
-      this.map = L.map('map').setView([20.7, -103.3], 12);
+      this.map = L.map('map').setView([20.7, -103.4], 11);
       this.tileLayer = L.tileLayer(
         'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
         {
@@ -113,34 +50,25 @@ export default {
         },
       );
       this.tileLayer.addTo(this.map);
+    },
+    getLandmarks() {
       axios(`http://localhost:8080/geodata/categories`, {method:"get"})
       .then(response => {
-        console.log(response.data);
+        response.data[0]["active"]=false;
+        response.data[1]["active"]=false;
+        this.layers=response.data;
+        this.initLayers();
       })
       .catch(e => {
       this.errors.push(e)
-      })
-      /*
-      const testURL = 'http://localhost:8080/geodata/categories';
-      const myInit = {
-        method: 'GET',
-        mode: 'no-cors',
-      };
-
-      const myRequest = new Request(testURL, myInit);
-
-      fetch(myRequest).then(function(response) {
-        console.log(response);
-      }).catch(function(e){
-        console.log(e);
-      });*/
+      });
     },
     initLayers() {
       this.layers.forEach((layer) => {
         const markerFeatures = layer.features.filter(feature => feature.type === 'marker');
         markerFeatures.forEach((feature) => {
           feature.leafletObject = L.marker(feature.coords)
-            .bindPopup("<h3>"+feature.name+"</h3><br>"+feature.name);
+            .bindPopup("<h3>"+feature.name+"</h3><br>"+feature.description);
         });
       });
     },

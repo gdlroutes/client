3c07dbd77ce9d9dcf633c9ed3fb24e4f3be42577
chuyalvarez <jesusaagomez@hotmail.com>
2019-03-04 13:34:33 -0600

    removed greetings dependencies

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,12 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
-import GreetingPage from './components/GreetingPage.vue';
 import Toolbar from './components/Toolbar.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    GreetingPage,
     Toolbar
   },
   data() {
